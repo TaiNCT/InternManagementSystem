a8cd833c86244041a15921eac5f21610ba5b2df9
Quang Nguyễn <107819768+KaruiClaudius@users.noreply.github.com>
2024-07-15 06:57:47 +0700

    fix

--- InternManagement/Pages/Interns/ManageInterns.cshtml.cs
+++ InternManagement/Pages/Interns/ManageInterns.cshtml.cs
@@ -1,11 +1,13 @@
 using IMSBussinessObjects;
 using IMSRepositories;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace InternManagement.Pages.Interns
 {
+    [Authorize(Roles = "Admin")]
     public class ManageInternModel : PageModel
     {
         private readonly ITeamRepository _teamRepository;
