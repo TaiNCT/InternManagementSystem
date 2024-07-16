a929eeb77b3ed3757842cc2f9d658e3c3c2ee1e5
ThanhTai <146913614+TaiNCT@users.noreply.github.com>
2024-07-17 00:08:15 +0700

    CRUD Post

---
+++ InternManagement/Pages/Admin/Create.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using IMSBussinessObjects;
+using IMSServices;
+
+namespace InternManagement.Pages.Admin
+{
+    public class CreateModel : PageModel
+    {
+        private readonly ICampaignService campaignService;
+
+        public CreateModel(ICampaignService campaignSer)
+        {
+            campaignService = campaignSer;
+        }
+
+        public IActionResult OnGet()
+        {
+        //ViewData["TeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName");
+            return Page();
+        }
+
+        [BindProperty]
+        public Campaign Campaign { get; set; } = default!;
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid || campaignService.GetCampaigns == null || Campaign == null)
+            {
+                return Page();
+            }
+            campaignService.AddCampaign(Campaign);
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ InternManagement/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using IMSBussinessObjects;
+using IMSServices;
+
+namespace InternManagement.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ICampaignService campaignService;
+
+        public IndexModel(ICampaignService campaignSer)
+        {
+            campaignService = campaignSer;
+        }
+
+        public IList<Campaign> Campaign { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (campaignService.GetCampaigns != null)
+            {
+                Campaign = campaignService.GetCampaigns();
+            }
+        }
+    }
+}
