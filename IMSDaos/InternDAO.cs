2430a99bb88fcba7ede27cef3f9a0d9eaf017bd2
Quang Nguyễn <107819768+KaruiClaudius@users.noreply.github.com>
2024-07-17 21:06:56 +0700

    Add Edit Intern

--- IMSDaos/InternDAO.cs
+++ IMSDaos/InternDAO.cs
@@ -82,13 +82,10 @@ namespace IMSDaos
                 existingIntern.Uni = newIntern.Uni;
                 existingIntern.Major = newIntern.Major;
                 existingIntern.Gpa = newIntern.Gpa;
-                existingIntern.TeamId = newIntern.TeamId;
                 existingIntern.Birthday = newIntern.Birthday;
                 existingIntern.CvUrl = newIntern.CvUrl;
                 existingIntern.PhotoUrl = newIntern.PhotoUrl;
-                existingIntern.OverallSuccess = newIntern.OverallSuccess;
-                existingIntern.InternshipStartingDate = newIntern.InternshipStartingDate;
-                existingIntern.InternshipEndingDate = newIntern.InternshipEndingDate;
+
                 db.Interns.Attach(existingIntern);
                 db.Entry(existingIntern).State = EntityState.Modified;
                 db.SaveChanges();
