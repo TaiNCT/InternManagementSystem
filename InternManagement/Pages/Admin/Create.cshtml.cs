a3aa60c0cc09eedf3221ddcafb8c8699d332aa45
ThanhTai <146913614+TaiNCT@users.noreply.github.com>
2024-07-17 14:02:48 +0700

    CreatePost

--- InternManagement/Pages/Admin/Create.cshtml.cs
+++ InternManagement/Pages/Admin/Create.cshtml.cs
@@ -7,36 +7,60 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using IMSBussinessObjects;
 using IMSServices;
+using System.Security.Claims;
 
 namespace InternManagement.Pages.Admin
 {
     public class CreateModel : PageModel
     {
         private readonly ICampaignService campaignService;
+        private readonly ITeamService teamService;  
+        private readonly IUserService userService;
 
-        public CreateModel(ICampaignService campaignSer)
+        public CreateModel(ICampaignService campaignSer, ITeamService teamSer, IUserService userSer)
         {
             campaignService = campaignSer;
+            teamService = teamSer;
+            userService= userSer;
         }
-
-        public IActionResult OnGet()
-        {
-        //ViewData["TeamId"] = new SelectList(_context.Teams, "TeamId", "TeamName");
-            return Page();
-        }
-
+       // [BindProperty]
+       // public int TeamId { get; set; }
         [BindProperty]
         public Campaign Campaign { get; set; } = default!;
-        
+        [BindProperty]
+        public List<SelectListItem> TeamSelectListItem { get; set; }
+        public async Task OnGetAsync()
+        {
+            TeamSelectListItem = teamService.GetAllTeams()
+                .Select(t => new SelectListItem
+                {
+                    Value = t.TeamId.ToString(),
+                    Text = t.TeamName
+                })
+                .ToList();
+        }
 
-        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
         {
-          if (!ModelState.IsValid || campaignService.GetCampaigns == null || Campaign == null)
+            var userEmailClaim = User.FindFirst(ClaimTypes.Email)?.Value;
+            if (!string.IsNullOrEmpty(userEmailClaim))
             {
-                return Page();
+                var user = userService.GetUsers().SingleOrDefault(x => x.Email == userEmailClaim);
+                string userName = null;
+                if (user.Username == null)
+                {
+                    userName = "Admin";
+                }
+                else
+                {
+                    userName = user.Username;
+                }
+                Campaign.StartDate = DateTime.Now;
+                Campaign.CreatedBy = userName;
+
+                campaignService.AddCampaign(Campaign);
+               
             }
-            campaignService.AddCampaign(Campaign);
             return RedirectToPage("./Index");
         }
     }
